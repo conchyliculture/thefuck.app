4b3452d0356ae966515421abcd1ee504c96f9365
renzo <github@renzokuken.eu>
2018-05-09 01:17:59 +0200

    refactor

--- thefuck.rb
+++ thefuck.rb
@@ -13,14 +13,11 @@ get '/' do
     subdomain = host.split(".")[0..-3].join(".")
     redir = $redir[subdomain]
     if redir
-        $stderr.puts redir
-        if redir=~/^.{11}$/
-            #YID
+        if redir=~/^.{11}$/ #YID
             redirect "https://www.youtube.com/embed/#{redir}?autoplay=1", 301
         else
             redirect redir, 301
         end
-    else
-        redirect "https://www.youtube.com/embed/nc_LIR5ExIU?autoplay=1", 301
     end
+    redirect "https://www.youtube.com/embed/nc_LIR5ExIU?autoplay=1", 301
 end
